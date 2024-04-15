b5de7e8166f9d2c414d6dce5d9e260d479eccc2d
KFH <KFH@LAPTOP-TRN-140>
2024-04-15 14:33:00 +0300

    Add project files.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("write your name:");
+string username = Console.ReadLine();
+GreetUser(username);
+void GreetUser(string name)
+{
+    Console.WriteLine("hello " + name);
+
+}
+Console.WriteLine("enter the measurment of the box:");
+Console.Write("Length:");
+double length = Convert.ToDouble(Console.ReadLine());
+Console.Write("width:");
+double width = Convert.ToDouble(Console.ReadLine());
+Console.Write("hight");
+double height = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("volume is " + height*width*length);
